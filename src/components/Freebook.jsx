0b6aad14d14d6bf4b9607e00b67ee93a185d3b58
Vinayak Bhawsar <admin@vinayak.com>
2026-02-16 21:30:02 +0530

    Fixed all production API URLs

--- src/components/Freebook.jsx
+++ src/components/Freebook.jsx
@@ -12,7 +12,7 @@ function Freebook() {
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get("https://bookstoreapp-master-ecsh.onrender.com");
+        const res = await axios.get("https://bookstoreapp-master-ecsh.onrender.com/book");
 
         const data = res.data.filter((data) => data.category === "Free");
         console.log(data);
