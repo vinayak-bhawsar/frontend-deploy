9c25e3b74c260d0c5a430d69b76501880120c3f0
Vinayak Bhawsar <admin@vinayak.com>
2026-02-16 21:11:03 +0530

    Updated frontend API connection and CORS setup

--- src/components/Freebook.jsx
+++ src/components/Freebook.jsx
@@ -12,7 +12,7 @@ function Freebook() {
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get("http://localhost:4001/book");
+        const res = await axios.get("https://bookstoreapp-master-ecsh.onrender.com");
 
         const data = res.data.filter((data) => data.category === "Free");
         console.log(data);
